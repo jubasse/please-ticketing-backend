712b00b5a5605b558b5ab8cf425ba2ddaf45801b
Julien METRAL <contact@julien-metral.fr>
2022-12-19 16:16:15 +0400

    Add docker config and kubernetes config

--- apps/tickets/src/main.ts
+++ apps/tickets/src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AuthModule } from './auth.module';
+import { TicketsModule } from './tickets.module';
 import cookieSession from 'cookie-session';
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { NestExpressApplication } from '@nestjs/platform-express/interfaces';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AuthModule);
+  const app = await NestFactory.create<NestExpressApplication>(TicketsModule);
   app.set('trust proxy', true);
   app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== "test",
-  }))
+  }));
   await app.listen(3000);
 }
 bootstrap();
